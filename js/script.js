ad0298bcb005161085d962d15bbdb1eb5a756b84
tomcoldham <>
2023-11-24 16:48:15 +0000

    added questions into JS file, will change questions later

--- js/script.js
+++ js/script.js
@@ -2,4 +2,86 @@ const question = document.getElementsByClassName(questiontext);
 const answer = document.getElementsByClassName(answerbuttons);
 const nextnextquestion = document.getElementsByClassName(nextquestion);
 
+let questions = [
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+    {
+        question: "What is the capital of France?",
+        choice1: Paris,
+        choice2: London,
+        choice3: Tokyo,
+        choice4: Amsterdam,
+        answer: 1
+    },
+];
 
