72dad5689de504b7db8820e0bd4a3f5fd985bb5a
tomcoldham <>
2023-11-24 16:39:56 +0000

    added const variables to javascript

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const question = document.getElementsByClassName(questiontext);
+const answer = document.getElementsByClassName(answerbuttons);
+const nextnextquestion = document.getElementsByClassName(nextquestion);
+
+
