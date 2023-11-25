71acff0d04ba01787015350204f36183bf9b2989
tomcoldham <>
2023-11-25 15:25:28 +0000

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,59 @@
+
 document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById('switch');
-    
-});
 const questions = [
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-    {
-        question: "What is the capital of France?",
-        choice1: "Paris",
-        choice2: "London",
-        choice3: "Tokyo",
-        choice4: "Amsterdam",
-        answer: 1
-    },
-];
+        {
+            question: "What is the capital of France?",
+            choices: [
+                { text: "Paris", correct: true },
+                { text: "Rome", correct: false },
+                { text: "London", correct: false },
+                { text: "Sydney", correct: false },
+            ]
+        },
+        {
+            question: "What is the capital of Italy?",
+            choices: [
+                { text: "Paris", correct: false },
+                { text: "Rome", correct: true },
+                { text: "London", correct: false },
+                { text: "Sydney", correct: false },
+            ]
+        },
+    ];
 
-const questionElement = document.getElementById("question-display");
-const answerButton = document.getElementsByClassName("answer-buttons");
-const nextQuestion = document.getElementsByClassName("next-question");
+    const questionElement = document.getElementById("question-display");
+    const answerButtons = document.getElementsByClassName("answer-buttons")[0];
+    const nextQuestion = document.getElementById("nextQuestion");
 
-let questionNumber = 0;
-let score = 0;
+    let questionNumber = 0;
+    let score = 0;
 
-function startQuiz() {
-    questionNumber = 0;
-    score = 0;
-    loadQuestion();
-}
-    
-function loadQuestion() {
-let currentQuestion = questions[questionNumber];
-let questionNo = questionNumber + 1;
-questionElement.innerHTML = questionNo + currentQuestion.question;
-}
+    function startQuiz() {
+        questionNumber = 0;
+        score = 0;
+        loadQuestion();
+    }
 
-startQuiz()
+    function loadQuestion() {
+        resetState();
+        let currentQuestion = questions[questionNumber];
+        let questionNo = questionNumber + 1;
+        questionElement.innerHTML = questionNo + " ) " + currentQuestion.question;
+
+        currentQuestion.choices.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("answer-buttons");
+            answerButtons.appendChild(button);
+        });
+    }
+
+    function resetState() {
+        nextQuestion.style.display = 'none';
+        while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+        }
+    }
+
+    startQuiz();
+});
